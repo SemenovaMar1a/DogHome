2c27f774365b3c09aff2d9c465dfcb2811d0f3b4
Маша <Semenova.semyono@yandex.ru>
2023-10-24 09:08:26 +0300

    version0.5

---
+++ DogHome/MainPage/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.views.generic import TemplateView
+
+from .views import *
+
+urlpatterns = [
+    path('', Main.as_view(), name='main'),
+    path('dog_cards/', DogCard.as_view(), name='dogCards'),
+    path('shelter/', ShelterCard.as_view(), name='shelterDog'),
+    path('dog_cards/<int:pk>', ViewDog.as_view(), name='viewdog'),
+    path('filter/<int:shelter_id>', ShelterByDogs.as_view(), name='shelterbydogs'),
+    path('shelter/<int:pk>', ViewShelter.as_view(), name='viewshelter'),
+]
